1be67afd50b538cea0ee67786e1769c707d9ee36
mczaf2008 <97153250+mczaf2008@users.noreply.github.com>
2022-01-14 21:18:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// var name1 = +prompt('1');
+// var name2 = +prompt('2');
+// var name3 = +prompt('3');
+
+// alert(name1 * name2 * name3);
+
+// alert(namel);
+
+// document.write(namel);
+
+// console.log(namel);
+
+
+var name1 = prompt('Ismiz');
+var name2 = prompt('Yoshiz');
+var name3 = prompt('Yilingiz');
+
+document.write(' Name - ' + name1 + ' Age - ' + name2 + ' Year - ' + name3);
